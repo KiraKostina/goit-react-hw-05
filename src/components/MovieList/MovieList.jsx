66ae423eb74f8cb2c0c44740b63b1a204c189d70
KiraKostina <kostina.kira@gmail.com>
2024-05-23 00:18:26 +0300

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,9 @@
-export default function MovieList({ movies }) {
+export default function MovieList({ movies = []}) {
     return (
         <ul>
             {movies.map((movie) => (
                 <li key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                    {/* <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} /> */}
                     <h3>{movie.title}</h3>
                 </li>
             ))}
