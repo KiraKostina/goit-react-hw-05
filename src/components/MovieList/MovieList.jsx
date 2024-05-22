90bf54de05726665f03eeb8485c5df4ca0e07d1f
KiraKostina <kostina.kira@gmail.com>
2024-05-23 00:39:12 +0300

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,14 @@
-export default function MovieList({movies}) {
+import { Link } from "react-router-dom";
+export default function MovieList({ movies }) {
     return (
         <ul>
             {movies.map((movie) => (
                 <li key={movie.id}>
                     {/* <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} /> */}
-                    <h3>{movie.title}</h3>
+                    <Link to={`/movies/${movie.id}`}>
+                        <h3>{movie.title}</h3>
+                    </Link>
+                    
                 </li>
             ))}
         </ul>
